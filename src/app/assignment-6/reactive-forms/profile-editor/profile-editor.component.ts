8281f552a845399b44d44b7af584dacab9501d55
karthikeyankumbam <karthikeyankumbam123@gmail.com>
2024-09-25 21:12:16 +0530

    added custom Validator

--- src/app/assignment-6/reactive-forms/profile-editor/profile-editor.component.ts
+++ src/app/assignment-6/reactive-forms/profile-editor/profile-editor.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators, AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
@@ -12,7 +12,7 @@ export class ProfileEditorComponent {
   // Nested Forms 
   profileForm = new FormGroup({
     firstName: new FormControl('',[Validators.required]),
-    lastName: new FormControl(''),
+    lastName: new FormControl('',customValidator()),
     address: new FormGroup({
       street: new FormControl(''),
       city: new FormControl(''),
@@ -23,4 +23,13 @@ export class ProfileEditorComponent {
   onSubmit() {
     console.log(this.profileForm.value);
   }
+}
+export function customValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value=control.value;
+    // console.log(value);
+    const isValid=value=="lastname" ? null : { customError: true };
+    // console.log(isValid);
+    return isValid;
+  };
 }
